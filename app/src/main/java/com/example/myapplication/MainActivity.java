b6f242a6ec72e06bfa58b35c21054711ab9a1279
Damncode12 <damncode12@gmail.com>
2023-05-04 16:51:20 +0530

    add: MAD_LAB LAB_ALERT_BOX ✅

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        new AlertDialog.Builder(this)
+                .setIcon(R.drawable.baseline_thumb_up_24)
+                .setTitle("Performance ")
+                .setMessage("You are getting better at scoring with time.")
+                        .setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(getApplicationContext(), "Hello",Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
